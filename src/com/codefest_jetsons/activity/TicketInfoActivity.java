0165d1ea1dd13a10090e1ffdf98e3a9b0e698362
Abhijit Nukalapati <abhijit@brandingbrand.com>
2013-02-24 04:06:14 -0500

    add time view

--- src/com/codefest_jetsons/activity/TicketInfoActivity.java
+++ src/com/codefest_jetsons/activity/TicketInfoActivity.java
@@ -1,18 +1,25 @@
 package com.codefest_jetsons.activity;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
@@ -41,6 +48,7 @@ public class TicketInfoActivity extends Activity {
     private int lastH;
     private CountDownTimer countDownTimer;
     private Ticket t;
+    private Button mAddTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -111,6 +119,15 @@ public class TicketInfoActivity extends Activity {
                 "Nick", "Tate", "4300112233445566", "09", "2014", "554", CreditCard.CreditCardType.MASTER_CARD);
         CreditCard cc = ParkingSharedPref.getCreditCard(mAppContext, "ntate@gmail.com", id);
         */
+        
+        mAddTime = (Button) findViewById(R.id.add_time);
+        mAddTime.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				MyDialogFragment df = new MyDialogFragment();
+				df.show(getFragmentManager(), null);
+			}
+		});
     }
 
     private void setListeners() {
@@ -226,4 +243,97 @@ public class TicketInfoActivity extends Activity {
         }.start();
     }
 
+    private class MyDialogFragment extends DialogFragment implements SeekBar.OnSeekBarChangeListener{
+
+    	private final int SNAP_DELTA_MINUTES = 15;
+    	private final double COST_PER_MINUTE = 0.01666666666666;
+    	private SeekBar mTimeBar;
+    	private TextView mHour;
+    	private TextView mMinute;
+    	private TextView mClock;
+    	private Button mPark;
+    	
+    	private final int mMaxtimeSeconds = 7200; // maximum time in seconds the user can choose
+    	
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			getDialog().setTitle("Add Time");
+			
+			View v = inflater.inflate(R.layout.add_time_fragment, null);
+			
+			mTimeBar = (SeekBar) v.findViewById(R.id.park_time);
+			mHour = (TextView) v.findViewById(R.id.hour);
+			mMinute = (TextView) v.findViewById(R.id.minute);
+			mClock = (TextView) v.findViewById(R.id.clock);
+			mPark = (Button) v.findViewById(R.id.park);
+			mPark.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					getDialog().dismiss();
+				}
+			});
+			
+			mTimeBar.setMax((mMaxtimeSeconds/60)/SNAP_DELTA_MINUTES);
+			mTimeBar.setOnSeekBarChangeListener(this);
+			
+			return v;
+		}
+
+		/**
+		 * 
+		 * @param calendar
+		 *            - a Calendar object
+		 */
+		private void setClockTime(Calendar calendar) {
+			int period = calendar.get(Calendar.AM_PM);
+			String am_pm = (period == Calendar.AM) ? "AM" : "PM";
+			int clock_hour = calendar.get(Calendar.HOUR_OF_DAY) % 12;
+			
+			// clock hour is 0. implies that the current hour is 12
+			if(clock_hour == 0) 
+				clock_hour = 12;
+			
+			int clock_minute = calendar.get(Calendar.MINUTE);
+			mClock.setText(String.format("%02d:%02d %s", clock_hour, clock_minute,
+					am_pm));
+		}
+		
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			progress *= SNAP_DELTA_MINUTES;
+			// calculate the seconds moved
+			// 100 is the max progress, by default
+			int secondsMoved = progress * 60;//(progress * mMaxtimeSeconds) / 100;
+
+			// update the time
+			int minutes = (secondsMoved / 60) % 60;
+			int hours = (secondsMoved / (60 * 60)) % 24;
+			mMinute.setText(String.format("%02d", minutes));
+			mHour.setText(String.format("%02d", hours));
+			
+			// Update the cost
+			double cost = COST_PER_MINUTE * progress;
+			String costString = java.text.NumberFormat.getCurrencyInstance().format(cost);
+			mPark.setText("PARK (" +costString+")");
+
+			// update the clock
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(System.currentTimeMillis()
+					+ (secondsMoved * 1000));
+			setClockTime(calendar);
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+			// do nothing
+			
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			// do nothing
+		}
+		
+    }
 }
