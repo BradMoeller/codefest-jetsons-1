198fb0171081d60a0aef9e1fd9c1ba7b810f7ca5
Nicholas Tate <ntate22@gmail.com>
2013-02-23 23:11:49 -0500

    layout updates

--- src/com/codefest_jetsons/fragment/AddCardFragment.java
+++ src/com/codefest_jetsons/fragment/AddCardFragment.java
@@ -1,5 +1,7 @@
 package com.codefest_jetsons.fragment;
 
+import android.content.Intent;
+import android.widget.Button;
 import com.codefest_jetsons.R;
 
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.codefest_jetsons.activity.TicketInfoActivity;
 
 public class AddCardFragment extends Fragment {
 
@@ -19,7 +22,15 @@ public class AddCardFragment extends Fragment {
 			Bundle savedInstanceState) {
 		
 		View view = inflater.inflate(R.layout.add_card_fragment, null);
-		
+
+        Button park = (Button) view.findViewById(R.id.add_card_park);
+        park.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), TicketInfoActivity.class));
+            }
+        });
+
 		return view;
 	}
 }
